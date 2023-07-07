00a9dde9cdc0d942a3ec93bd03c46fd17fb99de0
CaioMVargas <caiomvargas@gmail.com>
2023-07-07 16:00:11 -0300

    construcao Header e Sidebar

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,17 @@
+import styles from './Sidebar.module.css';
+
+export function Sidebar() {
+    return(
+        <aside className={styles.sidebar}>
+            <img className={styles.cover} src="https://images.unsplash.com/photo-1592609931095-54a2168ae893?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=50" alt="Imagem de Capa" />
+            <div className={styles.profile}>
+                <strong>Nome</strong>
+                <span>Cargo</span>
+            </div>
+
+            <footer>
+                <a href="#">Editar seu perfil</a>
+            </footer>
+        </aside>
+    );
+}
